d791be160619f87ddbc11ecddb8c73639e2ced36
jaydarschool <105365291+jaydarschool@users.noreply.github.com>
2022-05-13 12:27:33 -0400

    Update DOM-processing.js

--- DOM-processing.js
+++ DOM-processing.js
@@ -1,67 +1,64 @@
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script>
-        //don't worry too much about these variables
-        var num1In = document.getElementById("num1In");
-        var operIn = document.getElementById("operIn");
-        var num2In = document.getElementById("num2In");
-        var calcBtn = document.getElementById("calcButton");
-        var probTxt = document.getElementById("probDiv");
-        var resetBtn = document.getElementById("resButton");
-        var histr = document.getElementById("hist");
-        var clrBtn = document.getElementById("clearButton");
+        var $num1In = $("#num1In");
+        var $operIn = $("#operIn");
+        var $num2In = $("#num2In");
+        var $calcBtn = $("#calcButton");
+        var $probTxt = $("#probDiv");
+        var $resBtn = $("#resButton");
+        var $histr = $("#hist");
+        var $clrBtn = $("#clearButton");
         
         var onSub = function() { //this function estimates the problem
-            if (parseFloat(num1In.value) === 0 || parseFloat(num2In.value) === 0 && operIn.value === "divis") { //if the operation is division and you try to put in a 0 anywhere:
+            if (parseFloat($num1In.val()) === 0 || parseFloat($num2In.val()) === 0 && $operIn.val() === "divis") { //if the operation is division and you try to put in a 0 anywhere:
                 reset(); //reset the form
                 window.alert("Dividing with 0 doesn't work, nice try."); //give the user an error
-            } else if (num1In.value === "" || num2In.value === "") { //but then if at least one number value is empty:
+            } else if ($num1In.val() === "" || $num2In.val() === "") { //but then if at least one number value is empty:
                 reset(); //reset the form
                 window.alert("Sorry, you gotta input 2 numbers. That's just the rules."); //give the user an error
             } else { //otherwise, if there's no errors;
-                probTxt.textContent = num1In.value; //changes the problem's text to the first number
-                var operStr = ""; //this is the variable for the operation, turned into a string, and this variable will be used a sec, literaly
-                if (operIn.value === "addit") { //if the operation is addition:
+                $probTxt.text($num1In.val()); //changes the problem's text to the first number
+                var operStr; //this is the variable for the operation, turned into a string, and this variable will be used a sec, literaly
+                if ($operIn.val() === "addit") { //if the operation is addition:
                     operStr = " + "; //set the value of operStr to the + sign
-                } else if (operIn.value === "subtr") { //but then if the operation is subtraction:
+                } else if ($operIn.val() === "subtr") { //but then if the operation is subtraction:
                     operStr = " - "; //set the value of operStr to the - sign
-                } else if (operIn.value === "multi") { //but then if the operation is multiplication:
+                } else if ($operIn.val() === "multi") { //but then if the operation is multiplication:
                     operStr = " x "; //set the value of operStr to the multiplication sign
                 } else { //otherwise, the operation must be division, so:
                     operStr = " / "; //set the value of operStr to the division sign
                 }
-                probTxt.textContent += operStr; //now add the value of operStr to the problem
+                $probTxt.append(operStr); //now add the value of operStr to the problem
                 
-                probTxt.textContent += num2In.value + " = "; //and then add the second number to the problem
+                $probTxt.append($num2In.val() + " = "); //and then add the second number to the problem
                 
-                var ans = 0; //now make a variable containing the answer, used in a sec, literally, again.
-                if (operIn.value === "addit") { //if the operation is addition:
-                    ans = parseFloat(num1In.value) + parseFloat(num2In.value); //the answer equals the first number plus the second number
-                } else if (operIn.value === "subtr") { //but then if the operation is subtraction:
-                    ans = num1In.value - num2In.value; //the answer equals the first number minus the second number
-                } else if (operIn.value === "multi") { //but then if the operation is multiplication:
-                    ans = num1In.value * num2In.value; //the answer equals the first number times the second number
+                var ans; //now make a variable containing the answer, used in a sec, literally, again.
+                if ($operIn.val() === "addit") { //if the operation is addition:
+                    ans = parseFloat(parseFloat($num1In.val()) + parseFloat($num2In.val())); //the answer equals the first number plus the second number
+                } else if ($operIn.val() === "subtr") { //but then if the operation is subtraction:
+                    ans = parseFloat(parseFloat($num1In.val()) - parseFloat($num2In.val())).toString(); //the answer equals the first number minus the second number
+                } else if ($operIn.val() === "multi") { //but then if the operation is multiplication:
+                    ans = parseFloat(parseFloat($num1In.val()) * parseFloat($num2In.val())).toString(); //the answer equals the first number times the second number
                 } else { //otherwise, the operation must be division, so:
-                    ans = num1In.value / num2In.value; //the answer equals the first number divided by the second number
+                    ans = parseFloat(parseFloat($num1In.val()) / parseFloat($num2In.val())).toString(); //the answer equals the first number divided by the second number
                 }
                 
-                probTxt.textContent += ans; //finally, add the answer to the problem
-                histr.innerHTML += "<br>" + num1In.value + operStr + num2In.value + " = " + ans;
+                $probTxt.append(ans); //finally, add the answer to the problem
+                $histr.append("<br>" + $num1In.val() + operStr + $num2In.val() + " = " + ans);
             }
         };
         
         var reset = function() { //this function just sets all the values to the defaults
-            num1In.value = "";
-            operIn.value = "addit";
-            num2In.value = "";
+            $num1In.val("");
+            $operIn.val("addit");
+            $num2In.val("");
         }
         
         var clear = function() {
-            probTxt.textContent = "(Math Problem Goes Here...)";
+            $probTxt.text("(Math Problem Goes Here...)");
         }
         
-        
-        calcBtn.addEventListener("click", onSub); //now, if the calculate button is pressed, calculate the problem
-        
-        clrBtn.addEventListener("click", clear);
-        
-        resetBtn.addEventListener("click", reset); //now, if the reset button is pressed, set all the values to defaults
+        $calcBtn.on("click", onSub);
+        $resBtn.on("click", reset);
+        $clrBtn.on("click", clear);
     </script>
